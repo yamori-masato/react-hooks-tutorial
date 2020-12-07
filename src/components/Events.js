a9b846f2adfb048fe44afae51e33cb1448406a29
yamori-masato <yamorimasato@gmail.com>
2020-12-08 04:24:40 +0900

    setup react context provider

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
 import Event from './Event'
+import AppContext from '../contexts/AppContext'
 
 const Events = ({ state, dispatch }) => {
+    const value = useContext(AppContext)
+    
     return (
-        <>      
+        <>
             <h4>イベント一覧</h4>
             <table className="table table-hover">
                 <thead>
