44952c800bb155ef05a49fcd87f91e6ed3e72b29
yamori-masato <yamorimasato@gmail.com>
2020-12-08 23:52:15 +0900

    refactor events with reducer

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -47,7 +47,7 @@ const EventFrom = () => {
                 <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)}/>
             </div>
             <button className="btn btn-primary" onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
-            <button className="btn btn-danger" onClick={deleteAllEvent} disabled={state.length === 0}>全てのイベントを削除する</button>
+            <button className="btn btn-danger" onClick={deleteAllEvent} disabled={state.events.length === 0}>全てのイベントを削除する</button>
             </form>
         </>
     )
